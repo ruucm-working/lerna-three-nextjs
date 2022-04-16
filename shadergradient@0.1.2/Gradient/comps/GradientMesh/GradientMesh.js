bd898ca7b0cee84da75cf1d3d1bb0c7fbaacfd84
deploy <deploy@users.noreply.github.com>
2022-04-16 14:57:49 +0000

    Updated

---
+++ shadergradient@0.1.2/Gradient/comps/GradientMesh/GradientMesh.js
@@ -0,0 +1,20 @@
+// src/Gradient/comps/GradientMesh/GradientMesh.tsx
+import * as React from "react";
+import { useRef } from "react";
+import { useFiber } from "../../../useFiber.js";
+var GradientMesh = () => {
+  const { useFrame } = useFiber();
+  const mesh = useRef();
+  useFrame((state, delta) => mesh.current.rotation.x += 0.01);
+  return /* @__PURE__ */ React.createElement("mesh", {
+    ref: mesh,
+    rotation: [0.3, 0.3, 0]
+  }, /* @__PURE__ */ React.createElement("boxGeometry", {
+    args: [1, 1, 1]
+  }), /* @__PURE__ */ React.createElement("meshStandardMaterial", {
+    color: "green"
+  }));
+};
+export {
+  GradientMesh
+};
