2ed887c5aabd2dcb72ddbdc6a44c9aefaae7ad29
ruucm <ruucm.a@gmail.com>
2022-04-16 23:21:08 +0900

    Clean up!

--- packages/shared/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button'
-export { FiberCanvas, FiberContextProvider } from './useFiber'
-export { Gradient } from './Gradient/index'
-export { GradientMesh } from './Gradient/comps/GradientMesh/index'
